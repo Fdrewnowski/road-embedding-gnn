5a2eeb78fabca39d0d46574f19dd0eeb860a4b49
Fdrewnowski <f.drewnowski@wp.pl>
2023-01-22 20:30:29 +0100

    add training pipline script

---
+++ train_models.sh
@@ -0,0 +1,10 @@
+# download data
+python ./download_graphs.py -t 
+python ./download_graphs.py -v
+# transform data and transform into dgl format
+python transform_graphs.py -a -p ./data/data_train -o ./data/data_train/training_graphs.bin
+python transform_graphs.py -a -p ./data/data_val -o ./data/data_val/validation_graphs.bin
+
+python ./gae_train.py --use_cfg #--full_pipline
+python ./dgi_train.py --use_cfg #--full_pipline
+python ./graphmae_train.py --use_cfg #--full_pipline
